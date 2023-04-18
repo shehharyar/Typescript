1fb8cf6657f40e9807a4139599f748f2c58ccb4c
shehharyar <sheharyarhassan1456@gmail.com>
2023-04-18 12:17:48 +0500

    implemented classes

--- Practice/classes_and_interfaces/src/app.js
+++ Practice/classes_and_interfaces/src/app.js
@@ -1,15 +1,128 @@
-var Department = /** @class */ (function () {
-    function Department(n) {
-        this.name = n;
-    }
-    Department.prototype.describe = function () {
-        console.log("Department: " + this.name);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Department = /** @class */ (function () {
+    //id should be initialized onece ----
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        //     name: string;
+        this.employees = [];
+        // this.name= n;
+    }
+    Department.createEmployee = function (name) {
+        return { name: name };
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    Department.fiscalYear = 2023;
     return Department;
 }());
-var info_Tech = new Department("Information Tech");
-info_Tech.describe();
-var accounting = new Department("Accounting");
-accounting.describe();
-var finance = new Department("Finance");
-finance.describe();
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, "IT") || this;
+        _this.admins = admins;
+        _this.admins = admins;
+        return _this;
+    }
+    ITDepartment.prototype.describe = function () {
+        console.log("IT Department = id ", this.id);
+    };
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, "Accounting") || this;
+        _this.reports = reports;
+        _this.lastReport = reports[0];
+        return _this;
+    }
+    AccountingDepartment.getInstance = function () {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment("acc/2", []);
+        return this.instance;
+    };
+    AccountingDepartment.prototype.describe = function () {
+        console.log("Accounting Department = id ", this.id);
+    };
+    Object.defineProperty(AccountingDepartment.prototype, "mostRecentReport", {
+        get: function () {
+            if (this.lastReport) {
+                return this.lastReport;
+            }
+            throw new Error("Reports not Found..");
+        },
+        set: function (value) {
+            if (!value) {
+                throw new Error("Please Pass in a valid value!");
+            }
+            this.addReport(value);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    };
+    AccountingDepartment.prototype.addEmployee = function (name) {
+        if (name === "Sheharyar") {
+            return;
+        }
+        this.employees.push(name);
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var employee = Department.createEmployee("Max");
+var year = Department.fiscalYear;
+console.log(employee, year);
+// const info_Tech = new Department("IT", "Information Tech");
+// info_Tech.describe();
+var it = new ITDepartment("acc/1", ["Sheharyar"]);
+it.describe();
+it.addEmployee("Sheharyar");
+it.addEmployee("Maximillian");
+it.printEmployeeInformation();
+console.log(it);
+// const accounting = new AccountingDepartment("acc/2", []);
+var accounting = AccountingDepartment.getInstance();
+var accounting2 = AccountingDepartment.getInstance();
+console.log(accounting, accounting2);
+accounting.mostRecentReport = "You have to enlist all the procedures....";
+// accounting.addReport("Something Went Wrong...");
+accounting.printReports();
+accounting.addEmployee("Sheharyar");
+accounting.addEmployee("Manu");
+accounting.printEmployeeInformation();
+// accounting.addReport("Required Transactions are performed.")
+console.log(accounting.mostRecentReport);
+console.log(accounting);
+//we can access the properties of class outside of the class because bydefault , it is public.
+// accounting.employees.push("Manu")
+// const accountingCopy={name : "Sheharyar",describe: accounting.describe};
+// accountingCopy.name;
+// accountingCopy.describe;
--- Practice/classes_and_interfaces/src/app.ts
+++ Practice/classes_and_interfaces/src/app.ts
@@ -1,19 +1,124 @@
-class Department {
-    name: string;
+abstract class Department {
+  static fiscalYear = 2023;
+  //     name: string;
+  protected employees: string[] = [];
 
+  //id should be initialized onece ----
+  constructor(protected readonly id: string, public name: string) {
+    // this.name= n;
+  }
+  abstract describe(this: Department): void;
 
-    constructor(n: string){
-        this.name= n;
-    }
-    describe(this : Department){
-        console.log("Department: " + this.name);
-    }
+  static createEmployee(name: string) {
+    return { name: name };
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-const info_Tech=new Department("Information Tech");
-info_Tech.describe();
-const accounting= new Department("Accounting");
-const accountingCopy={name : "Sheharyar",describe: accounting.describe};
-accountingCopy.name;
-accountingCopy.describe()
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
 
+  describe() {
+    console.log("IT Department = id ", this.id);
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  private static instance: AccountingDepartment;
+
+
+  private constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  static getInstance(){
+    if(AccountingDepartment.instance){
+      return this.instance;      
+    }
+    this.instance= new AccountingDepartment("acc/2",[]);
+    return this.instance;
+  }
+
+  describe() {
+    console.log("Accounting Department = id ", this.id);
+  }
+
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("Reports not Found..");
+  }
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please Pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+  addEmployee(name: string) {
+    if (name === "Sheharyar") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const employee = Department.createEmployee("Max");
+const year = Department.fiscalYear;
+
+console.log(employee, year);
+
+// const info_Tech = new Department("IT", "Information Tech");
+// info_Tech.describe();
+const it = new ITDepartment("acc/1", ["Sheharyar"]);
+it.describe();
+it.addEmployee("Sheharyar");
+it.addEmployee("Maximillian");
+it.printEmployeeInformation();
+
+console.log(it);
+
+// const accounting = new AccountingDepartment("acc/2", []);
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance();
+
+console.log(accounting, accounting2);
+
+accounting.mostRecentReport = "You have to enlist all the procedures....";
+// accounting.addReport("Something Went Wrong...");
+accounting.printReports();
+accounting.addEmployee("Sheharyar");
+accounting.addEmployee("Manu");
+accounting.printEmployeeInformation();
+// accounting.addReport("Required Transactions are performed.")
+console.log(accounting.mostRecentReport);
+console.log(accounting);
+//we can access the properties of class outside of the class because bydefault , it is public.
+
+// accounting.employees.push("Manu")
+
+// const accountingCopy={name : "Sheharyar",describe: accounting.describe};
+// accountingCopy.name;
+// accountingCopy.describe;
